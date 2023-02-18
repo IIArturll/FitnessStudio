9e2ede86bd65707d354f4496b8a80560cefe3662
Artur <sharam.artur@gmail.com>
2023-02-18 03:44:40 +0300

    made exceptions from these classes, added the ignore property for the superclass

--- src/main/java/core/exceptions/MultipleErrorResponse.java
+++ src/main/java/core/exceptions/MultipleErrorResponse.java
@@ -1,14 +1,32 @@
 package core.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class MultipleErrorResponse {
+@JsonPropertyOrder({"logref", "errors"})
+@JsonIgnoreProperties(value = {"cause", "localizedMessage", "stackTrace", "suppressed", "message"})
+public class MultipleErrorResponse extends Exception {
+    @JsonProperty(value = "logref")
     private String logref;
+    @JsonProperty(value = "errors")
     List<ErrorField> errors;
+
+    public MultipleErrorResponse(String logref) {
+        this.logref = logref;
+        errors = new ArrayList<>();
+    }
 
     public MultipleErrorResponse(String logref, List<ErrorField> errors) {
         this.logref = logref;
         this.errors = errors;
+    }
+
+    public void setLogref(String logref) {
+        this.logref = logref;
     }
 
     public String getLogref() {
--- src/main/java/core/exceptions/SingleErrorResponse.java
+++ src/main/java/core/exceptions/SingleErrorResponse.java
@@ -1,19 +1,27 @@
 package core.exceptions;
 
-public class SingleErrorResponse {
-    private String logref;
-    private String massage;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    public SingleErrorResponse(String logref, String massage) {
+@JsonPropertyOrder({"logref", "message"})
+@JsonIgnoreProperties(value = {"cause", "localizedMessage", "stackTrace","suppressed"})
+public class SingleErrorResponse extends Exception {
+    @JsonProperty(value = "logref", required = true)
+    private String logref;
+    @JsonProperty("message")
+    private String message;
+
+    public SingleErrorResponse(String logref, String message) {
         this.logref = logref;
-        this.massage = massage;
+        this.message = message;
     }
 
     public String getLogref() {
         return logref;
     }
 
-    public String getMassage() {
-        return massage;
+    public String getMessage() {
+        return message;
     }
 }
