5137e581fe76d0205d891a68091dc5443721b05e
Artur <sharam.artur@gmail.com>
2023-02-25 21:16:30 +0300

    update ddl

--- postgres/ddl/01_init_users_tables.sql
+++ postgres/ddl/01_init_users_tables.sql
@@ -3,14 +3,14 @@ CREATE SCHEMA IF NOT EXISTS fitness;
 CREATE TABLE IF NOT EXISTS fitness.user_status
 (
     id SMALLSERIAL NOT NULL,
-    status TEXT NOT NULL,
+    status TEXT NOT NULL UNIQUE,
     CONSTRAINT user_status_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS fitness.user_role
 (
     id SMALLSERIAL NOT NULL,
-    role TEXT NOT NULL,
+    role TEXT NOT NULL UNIQUE,
     CONSTRAINT user_role_pkey PRIMARY KEY (id)
 );
 
@@ -19,12 +19,12 @@ CREATE TABLE fitness.user
     uuid UUID NOT NULL,
     dt_create TIMESTAMP(6) WITH TIME ZONE NOT NULL,
     dt_update TIMESTAMP(6) WITH TIME ZONE NOT NULL,
-    mail TEXT NOT NULL,
+    mail TEXT NOT NULL UNIQUE,
     fio TEXT NOT NULL,
     role SMALLINT NOT NULL,
     status SMALLINT NOT NULL,
     password TEXT NOT NULL,
-    CONSTRAINT uuid PRIMARY KEY (uuid),
+    CONSTRAINT uuid_user PRIMARY KEY (uuid),
     FOREIGN KEY (role)
         REFERENCES fitness.user_role (id),
     FOREIGN KEY (status)
